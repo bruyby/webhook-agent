4aa8bb2725d6bddb3aecd94643869a4001498382
Caiton Canpintam <Caitson.Canpintam@orange-sonatel.com>
2020-06-09 09:02:09 +0000

    Projeto de Dialogflow OGB

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const dfff = require('dialogflow-fulfillment');
+
+app.get('/',(req, res)=>{
+    res.send('Serveur en ligne!');
+});
+
+//app.post('/',express.json(),(req, res)=>{
+    //const agent = new dfff.WebhookClient({
+     //   request : req,
+    //    response : res
+    //});
+
+   // function information (){
+   //     agent.add('Testando ficheiro');
+   // }
+
+   // var intentMap = new Map();
+   // intentMap.set('info', information);
+
+   // agent.handleRequest(intentMap);
+
+//})
+
+app.listen(3333, ()=> console.log("Serveur sur le port 3333"));
