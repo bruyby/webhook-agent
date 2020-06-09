129a74d2712fc84e558885148198329b79543f9b
Caiton Canpintam <Caitson.Canpintam@orange-sonatel.com>
2020-06-09 15:11:37 +0000

    Testando axios no projeto

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const dfff = require('dialogflow-fulfillment');
+const axios = require('axios');
+
+const port = 3333;
 
 app.get('/',(req, res)=>{
     res.send('Serveur en ligne!');
@@ -16,11 +19,24 @@ app.post('/',express.json(),(req, res)=>{
         agent.add('Testando ficheiro');
     }
 
+    function  ApiExternoDemo(agent){
+        const word = agent.parameters.palavras;
+              agent.add('Intente foi chamado : '+ word);
+        return axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`)
+        .then((result) => {
+            result.data.map(wordObj => {
+                agent.add(wordObj.word);
+            });
+        });
+        
+      }
+
     var intentMap = new Map();
     intentMap.set('info', information);
+    intentMap.set('ApiExternoDemo', ApiExternoDemo);
 
     agent.handleRequest(intentMap);
 
 })
 
-app.listen(3333, ()=> console.log("Serveur sur le port 3333"));
+app.listen(port, ()=> console.log("Serveur sur le port port"));
